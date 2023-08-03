6937e281ababe6e092c749db86f769c928e64146
Ibrohim Subkhanqulov <ibragim.sub+sourcetree@gmail.com>
2023-08-03 20:42:59 +0500

    Exception Handling

--- PaymentRepository.cs
+++ PaymentRepository.cs
@@ -8,22 +8,37 @@ namespace PaymentServiceDemo
 
         public List<CustomerPayment> LoadPaymentsFromDatabase()
         {
-            if (File.Exists(PaymentFilePath))
+            try
             {
-                string json = File.ReadAllText(PaymentFilePath);
-                return JsonSerializer.Deserialize<List<CustomerPayment>>(json);
+                if (File.Exists(PaymentFilePath))
+                {
+                    string json = File.ReadAllText(PaymentFilePath);
+                    return JsonSerializer.Deserialize<List<CustomerPayment>>(json);
+                }
+                else
+                {
+                    return new List<CustomerPayment>();
+                }
             }
-            else
+            catch (Exception ex)
             {
+                Console.WriteLine("Error loading payments from database: " + ex.Message);
                 return new List<CustomerPayment>();
             }
         }
 
         public void SavePaymentsToDatabase(List<CustomerPayment> payments)
         {
-            string json = JsonSerializer.Serialize(payments, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(PaymentFilePath, json);
+            try
+            {
+                string json = JsonSerializer.Serialize(payments, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(PaymentFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving payments to database: " + ex.Message);
+            }
         }
+
     }
 }
-
